7210b0cbc0c3e075da327b18e196f6e41f709565
Rens Dur <rens.dur@projectmarch.nl>
2025-11-20 18:59:22 +0100

    Start with large batch implementation

---
+++ numerical/python/compressed_domain.py
@@ -0,0 +1,114 @@
+import torch
+import math
+
+
+class CompressedDomain:
+
+
+    def __init__(self, width, height, meter_per_cell):
+        self.width = width
+        self.height = height
+
+        # All quantities are represented in this one big tensor
+        # 0. h
+        # 1. S
+        # 2. u
+        # 3. v
+        # 4. B
+        # 5. eta
+        # 6. u*h
+        # 7. v*h
+        # 8. Ds * dS_dx
+        # 9 .Ds * dS_dy
+        self.data = torch.zeros(10, self.height, self.width)
+
+        # Create identifiers for them (index-helpers)
+        self.h = 0
+        self.S = 1
+        self.u = 2
+        self.v = 3
+        self.B = 4
+        self.eta = 5
+        self.uh = 6
+        self.vh = 7
+        self.Ds_dS_dx = 8
+        self.Ds_dS_dy = 9
+
+        self.Hin = 1e-5
+        self.Hc = 1e-3
+        self.H0 = 0.02 # Initial water thickness
+        self.grav = 9.81
+        self.rho = 1000 # Water density
+        self.Du = 0.5 # Turbulent Eddy velocity
+        self.nb = 0.016 # bed roughness for bare land
+        self.nv = 0.2 # bed roughness for vegetated land
+        self.k = 1500 # Vegetation carrying capacity
+        self.D0 = 1e-7 # Sediment diffusivity in absence of vegetation
+        self.pD = 0.99 # fraction by which sediment diffusivity is reduced when vegetation is at carrying capacity
+        self.Sin = 5e-9 # Maximum sediment input rate
+        self.Qs = 6e-4 # water layer thickness at which sediment input is halved
+        self.Es = 2.5e-4 # Sediment erosion rate
+        self.pE = 0.9 # Fraction by which sediment erosion is reduced when vegetation is at carrying capacity
+        self.r = 3.2e-8 # Intrinsic plant growth rate (=1 per year)
+        self.Qq = 0.02 # Water layer thickness at which vegetation growth is halved
+        self.EB = 1e-5 # Vegetation erosion rate
+        self.DB = 6e-9 # Vegetation diffusivity
+        self.morphological_acc_factor = 44712 # Morphological acceleration factor, required for S and B
+        self.pEst = 0.002 # Probability of vegetation seedling establishment
+
+
+        # Domain properties
+        self.dx = meter_per_cell
+        self.dy = meter_per_cell
+        self.xlen = self.width * self.dx
+        self.ylen = self.height * self.dy
+
+    #
+    # INITIAL CONDITIONS (PRESETS)
+    #
+    def init_preset_gaussian_wave(self, x_mu, y_mu, x_sig, y_sig, correlation=0):
+        x = torch.linspace(0, self.xlen, self.width)
+        y = torch.linspace(0, self.ylen, self.height)
+        x, y = torch.meshgrid(x, y, indexing='xy')
+
+        A = 1 / (2*math.pi * x_sig * y_sig * (1-correlation**2)**0.5)
+
+        self.data[self.h, :, :] = 1 + A * torch.exp(
+            - (1/(2*(1-correlation**2))) * (((x-x_mu)/x_sig)**2 - 2*correlation*((x-x_mu)/x_sig)*((y-y_mu)/y_sig) + ((y-y_mu)/y_sig)**2)
+        )
+
+    def initialize(self):
+        # Sedimentary elevation is zero everywhere
+        self.data[self.S, :, :] = 0
+
+        # Flow velocities are zero everywhere
+        self.data[self.u, :, :] = 0
+        self.data[self.v, :, :] = 0
+
+        # Uniform water thickness H0
+        self.data[self.h, :, :] = self.H0
+
+        # Vegetation density is zero everywhere, except for some randomly placed tussocks
+        self.data[self.B, :, :] = 0
+        self.data[self.B, torch.where(torch.rand(self.height, self.width) < self.pEst)] = self.k
+
+    #
+    # MEMORY MANAGEMENT
+    #
+    def to(self, device):
+        self.data = self.data.to(device)
+
+    def get_h(self):
+        return self.data[self.h, :, :].cpu().numpy()
+
+    def get_S(self):
+        return self.data[self.S, :, :].cpu().numpy()
+
+    def get_B(self):
+        return self.data[self.B, :, :].cpu().numpy()
+
+    def get_u(self):
+        return self.data[self.u, :, :].cpu().numpy()
+
+    def get_v(self):
+        return self.data[self.v, :, :].cpu().numpy()
---
+++ numerical/python/compressed_numerical_solver.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+from compressed_domain import CompressedDomain
+
+
+class CompressedNumericalSolver:
+
+    def __init__(self):
+        self.device = torch.device('cpu')  # Default to CPU
+        # Switch to MPS (Apple Metal) if available
+        if torch.backends.mps.is_available():
+            self.device = torch.device('mps')
+        # Or CUDA if we're on an Nvidia machine
+        elif torch.cuda.is_available():
+            self.device = torch.device('cuda')
+
+        print(f"Using torch device '{self.device}'")
+
+        # Domain
+        self.domain = CompressedDomain(200, 200, 0.5)
+        self.domain.initialize()
+        self.domain.to(self.device)
+
+        # Timestep
+        self.dt = 0.0125
+        self.t = 0
+
+        # Parepare torch models
+        self.dx_kernel = torch.tensor([[[[0, 0, 0], [-1, 0, 1], [0, 0, 0]]]], dtype=torch.float32, device=self.device)
+        self.dx_conv = nn.Conv2d(1, 1, kernel_size=3, padding=1, padding_mode='replicate', groups=10, bias=False).to(self.device)
+        self.dx_conv.weight = torch.nn.Parameter(self.dx_kernel)
+
+        self.dy_kernel = torch.tensor([[[[0, -1, 0], [0, 0, 0], [0, 1, 0]]]], dtype=torch.float32, device=self.device)
+        self.dy_conv = nn.Conv2d(1, 1, kernel_size=3, padding=1, padding_mode='replicate', groups=10, bias=False).to(self.device)
+        self.dy_conv.weight = torch.nn.Parameter(self.dy_kernel)
+
+        self.dx2_kernel = torch.tensor([[[[0, 0, 0], [1, -2, 1], [0, 0, 0]]]], dtype=torch.float32, device=self.device)
+        self.dx2_conv = nn.Conv2d(1, 1, kernel_size=3, padding=1, padding_mode='replicate', groups=10, bias=False).to(self.device)
+        self.dx2_conv.weight = torch.nn.Parameter(self.dx2_kernel)
+
+        self.dy2_kernel = torch.tensor([[[[0, 1, 0], [0, -2, 0], [0, 1, 0]]]], dtype=torch.float32, device=self.device)
+        self.dy2_conv = nn.Conv2d(1, 1, kernel_size=3, padding=1, padding_mode='replicate', groups=10, bias=False).to(self.device)
+        self.dy2_conv.weight = torch.nn.Parameter(self.dy2_kernel)
+
+    def d_dx(self, data):
+        # Pack and unpack the 'data' tensor to match expected size by the convolution,
+        # then divide by twice the dx of our domain, because we take the derivative over this cell, using both neighbors.
+        return self.dx_conv(data.unsqueeze(0)).detach().squeeze() / (2 * self.domain.dx)
+
+    def d_dy(self, data):
+        # Pack and unpack the 'data' tensor to match expected size by the convolution,
+        # then divide by twice the dy of our domain, because we take the derivative over this cell, using both neighbors.
+        return self.dy_conv(data.unsqueeze(0)).detach().squeeze() / (2 * self.domain.dy)
+
+    def d2_dx2(self, data):
+        # Second partial derivative central difference
+        return self.dx2_conv(data.unsqueeze(0)).detach().squeeze() / (self.domain.dx ** 2)
+
+    def d2_dy2(self, data):
+        # Second partial derivative central difference
+        return self.dy2_conv(data.unsqueeze(0)).detach().squeeze() / (self.domain.dy ** 2)
+
+    def solve_step(self):
+
+        # Compute d/dx and d/dy for all quantities
+        d_dx = self.d_dx(self.domain.data)
+        d_dy = self.d_dy(self.domain.data)
+
+        # Compute d2/dx2 and d2/dy2 for all quantities
+        d2_dx2 = self.d2_dx2(self.domain.data)
+        d2_dy2 = self.d2_dy2(self.domain.data)
+
+        
