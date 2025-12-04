934ecfff27c76d610f52fd12c70a90d0370766fd
Rens Dur <rens.dur@projectmarch.nl>
2025-12-04 17:19:52 +0100

    Adjust training routine for saltmarsh

--- learning/PCNN-python/pcnn_solver.py
+++ learning/PCNN-python/pcnn_solver.py
@@ -136,39 +136,32 @@ class PCNNSolver:
             for i in range(self.params.n_batches_per_epoch):
 
                 # Ask for a batch from the dataset
-                h_old, u_old, v_old = self.dataset.ask()
+                h_old, u_old, v_old, S_old, B_old = self.dataset.ask()
 
                 # TODO: MAC grid
 
                 # Predict the new domain state by performing a forward pass through the network
-                h_new, u_new, v_new = self.net(h_old, u_old, v_old)
-
-                # S_new = S_old
-                # B_new = B_old
+                h_new, u_new, v_new, S_new, B_new = self.net(h_old, u_old, v_old, S_old, B_old)
 
                 # Choose between explicit, implicit or IMEX integration schemes
                 if self.params.integrator == "explicit":
                     h = h_old
                     u = u_old
                     v = v_old
-                    # S = S_old
-                    # B = B_old
+                    S = S_old
+                    B = B_old
                 elif self.params.integrator == "implicit":
                     h = h_new
                     u = u_new
                     v = v_new
-                    # S = S_new
-                    # B = B_new
+                    S = S_new
+                    B = B_new
                 else: # "imex", default
                     h = (h_new + h_old) / 2
                     u = (u_new + u_old) / 2
                     v = (v_new + v_old) / 2
-                    # S = (S_new + S_old) / 2
-                    # B = (B_new + B_old) / 2
-
-                # TODO: REMOVE!!
-                S = torch.zeros_like(h).to(self.device)
-                B = torch.zeros_like(h).to(self.device)
+                    S = (S_new + S_old) / 2
+                    B = (B_new + B_old) / 2
 
                 #
                 # COMPUTE LOSS
@@ -178,65 +171,65 @@ class PCNNSolver:
                 dh_dt = (h_new - h_old) / self.params.dt
                 du_dt = (u_new - u_old) / self.params.dt
                 dv_dt = (v_new - v_old) / self.params.dt
-                # dS_dt = (S_new - S_old) / self.params.dt
-                # dB_dt = (B_new - B_old) / self.params.dt
+                dS_dt = (S_new - S_old) / self.params.dt
+                dB_dt = (B_new - B_old) / self.params.dt
 
-                # # Compute bed roughness
-                # n = self.params.nb + (self.params.nv - self.params.nb) * (B / self.params.k)
-                #
-                # # Compute Chezy coefficient using Manning's formulation
-                # Cz = (1.0 / n) * torch.pow(h, 1.0 / 6.0)
-                #
-                # # Bed shear stress components in x and y direction
-                # bed_shear_stress_precalc = self.params.grav / torch.pow(Cz, 2.0) * torch.pow(u*u + v*v, 0.5)
-                # tau_bx_per_rho = bed_shear_stress_precalc * u
-                # tau_by_per_rho = bed_shear_stress_precalc * v
-                # tau_b_per_rho = self.params.grav / torch.pow(Cz, 2.0) * (u*u + v*v)
-                #
-                # # The topographic diffusion term requires extra attention
-                # Ds = self.params.D0 * (1.0 - self.params.pD * (B / self.params.k))
-                #
-                # topographic_diffusion_term = self.d_dx(Ds * self.d_dx(S)) + self.d_dy(Ds * self.d_dy(S))
-                #
-                # # Effective water layer thickness he
-                # he = h - self.params.Hc
+                # Compute bed roughness
+                n = self.params.nb + (self.params.nv - self.params.nb) * (B / self.params.k)
+                
+                # Compute Chezy coefficient using Manning's formulation
+                Cz = (1.0 / n) * torch.pow(h, 1.0 / 6.0)
+                
+                # Bed shear stress components in x and y direction
+                bed_shear_stress_precalc = self.params.grav / torch.pow(Cz, 2.0) * torch.pow(u*u + v*v, 0.5)
+                tau_bx_per_rho = bed_shear_stress_precalc * u
+                tau_by_per_rho = bed_shear_stress_precalc * v
+                tau_b_per_rho = self.params.grav / torch.pow(Cz, 2.0) * (u*u + v*v)
+                
+                # The topographic diffusion term requires extra attention
+                Ds = self.params.D0 * (1.0 - self.params.pD * (B / self.params.k))
+                
+                topographic_diffusion_term = self.d_dx(Ds * self.d_dx(S)) + self.d_dy(Ds * self.d_dy(S))
+                
+                # Effective water layer thickness he
+                he = h - self.params.Hc
 
                 # 1. Continuity loss
                 loss_h = torch.mean(self.loss_function(
-                    dh_dt + self.d_dx(u * h) + self.d_dy(v * h) # - self.params.Hin
+                    dh_dt + self.d_dx(u * h) + self.d_dy(v * h) - self.params.Hin
                 ), dim=(1,2,3))
 
                 # 2. Momentum loss
                 loss_u = du_dt + self.params.grav * self.d_dx(h + S) + u * self.d_dx(u) + v * self.d_dy(u)
                 loss_v = dv_dt + self.params.grav * self.d_dy(h + S) + u * self.d_dx(v) + v * self.d_dy(v)
 
-                # # Add bed friction effects
-                # loss_u += tau_bx_per_rho / h
-                # loss_v += tau_by_per_rho / h
-                #
-                # # Add turbulent mixing effects
-                # loss_u -= self.params.Du * (self.d2_dx2(u) + self.d2_dy2(u))
-                # loss_v -= self.params.Du * (self.d2_dx2(v) + self.d2_dy2(v))
+                # Add bed friction effects
+                loss_u += tau_bx_per_rho / h
+                loss_v += tau_by_per_rho / h
+                
+                # Add turbulent mixing effects
+                loss_u -= self.params.Du * (self.d2_dx2(u) + self.d2_dy2(u))
+                loss_v -= self.params.Du * (self.d2_dx2(v) + self.d2_dy2(v))
 
                 # Apply loss function and compute mean
                 loss_u = torch.mean(self.loss_function(loss_u), dim=(1,2,3))
                 loss_v = torch.mean(self.loss_function(loss_v), dim=(1,2,3))
 
-                # # 3. Sediment loss
-                # loss_S = torch.mean(self.loss_function(
-                #     dS_dt \
-                #     - self.params.Sin * (he / (self.params.Qs + he)) \
-                #     + self.params.Es * (1.0 - self.params.pE * (B / self.params.k)) * S * tau_b_per_rho \
-                #     - topographic_diffusion_term
-                # ), dim=(1,2,3))
-                #
-                # # 4. Vegetation stem density loss
-                # loss_B = torch.mean(self.loss_function(
-                #     dB_dt \
-                #     - self.params.r * B * (1.0 - (B / self.params.k)) * (self.params.Qq / (self.params.Qq + he)) \
-                #     + self.params.EB * B * tau_b_per_rho \
-                #     - self.params.DB * (self.d2_dx2(B) + self.d2_dy2(B))
-                # ), dim=(1,2,3))
+                # 3. Sediment loss
+                loss_S = torch.mean(self.loss_function(
+                    dS_dt \
+                    - self.params.Sin * (he / (self.params.Qs + he)) \
+                    + self.params.Es * (1.0 - self.params.pE * (B / self.params.k)) * S * tau_b_per_rho \
+                    - topographic_diffusion_term
+                ), dim=(1,2,3))
+                
+                # 4. Vegetation stem density loss
+                loss_B = torch.mean(self.loss_function(
+                    dB_dt \
+                    - self.params.r * B * (1.0 - (B / self.params.k)) * (self.params.Qq / (self.params.Qq + he)) \
+                    + self.params.EB * B * tau_b_per_rho \
+                    - self.params.DB * (self.d2_dx2(B) + self.d2_dy2(B))
+                ), dim=(1,2,3))
 
                 # 5. Boundary loss
 
@@ -250,23 +243,23 @@ class PCNNSolver:
                 loss_bound_left += torch.mean(self.loss_function(
                     h[:, :, :, 0] - h[:, :, :, 1]
                 ))
-                # loss_bound_left += torch.mean(self.loss_function(
-                #     S[:, :, :, 0] - S[:, :, :, 1]
-                # ))
-                # loss_bound_left += torch.mean(self.loss_function(
-                #     B[:, :, :, 0] - B[:, :, :, 1]
-                # ))
+                loss_bound_left += torch.mean(self.loss_function(
+                    S[:, :, :, 0] - S[:, :, :, 1]
+                ))
+                loss_bound_left += torch.mean(self.loss_function(
+                    B[:, :, :, 0] - B[:, :, :, 1]
+                ))
 
-                # Closed boundary on the right
-                loss_bound_right = torch.mean(self.loss_function(
-                    u[:, :, :, -1] + u[:, :, :, -2]
-                ))
-                loss_bound_right += torch.mean(self.loss_function(
-                    v[:, :, :, -1] - v[:, :, :, -2]
-                ))
-                loss_bound_right += torch.mean(self.loss_function(
-                    h[:, :, :, -1] - h[:, :, :, -2]
-                ))
+                # # Closed boundary on the right
+                # loss_bound_right = torch.mean(self.loss_function(
+                #     u[:, :, :, -1] + u[:, :, :, -2]
+                # ))
+                # loss_bound_right += torch.mean(self.loss_function(
+                #     v[:, :, :, -1] - v[:, :, :, -2]
+                # ))
+                # loss_bound_right += torch.mean(self.loss_function(
+                #     h[:, :, :, -1] - h[:, :, :, -2]
+                # ))
                 # loss_bound_right += torch.mean(self.loss_function(
                 #     S[:, :, :, -1] - S[:, :, :, -2]
                 # ))
@@ -274,22 +267,22 @@ class PCNNSolver:
                 #     B[:, :, :, -1] - B[:, :, :, -2]
                 # ))
 
-                # Open boundary on the right
-                # loss_bound_right = torch.mean(self.loss_function(
-                #     u[:, :, :, -1] - 2*u[:, :, :, -2] + u[:, :, :, -3]
-                # ))
-                # loss_bound_right += torch.mean(self.loss_function(
-                #     v[:, :, :, -1] - 2*v[:, :, :, -2] + v[:, :, :, -3]
-                # ))
-                # loss_bound_right += torch.mean(self.loss_function(
-                #     h[:, :, :, -1] - h[:, :, :, -2]
-                # ))
-                # loss_bound_right += torch.mean(self.loss_function(
-                #     S[:, :, :, -1]
-                # ))
-                # loss_bound_right += torch.mean(self.loss_function(
-                #     B[:, :, :, -1] - B[:, :, :, -2]
-                # ))
+                Open boundary on the right
+                loss_bound_right = torch.mean(self.loss_function(
+                    u[:, :, :, -1] - 2*u[:, :, :, -2] + u[:, :, :, -3]
+                ))
+                loss_bound_right += torch.mean(self.loss_function(
+                    v[:, :, :, -1] - 2*v[:, :, :, -2] + v[:, :, :, -3]
+                ))
+                loss_bound_right += torch.mean(self.loss_function(
+                    h[:, :, :, -1] - h[:, :, :, -2]
+                ))
+                loss_bound_right += torch.mean(self.loss_function(
+                    S[:, :, :, -1]
+                ))
+                loss_bound_right += torch.mean(self.loss_function(
+                    B[:, :, :, -1] - B[:, :, :, -2]
+                ))
 
                 # Closed boundary at the top
                 loss_bound_top = torch.mean(self.loss_function(
@@ -301,12 +294,12 @@ class PCNNSolver:
                 loss_bound_top += torch.mean(self.loss_function(
                     h[:, :, 0, :] - h[:, :, 1, :]
                 ))
-                # loss_bound_top += torch.mean(self.loss_function(
-                #     S[:, :, 0, :] - S[:, :, 1, :]
-                # ))
-                # loss_bound_top += torch.mean(self.loss_function(
-                #     B[:, :, 0, :] - B[:, :, 1, :]
-                # ))
+                loss_bound_top += torch.mean(self.loss_function(
+                    S[:, :, 0, :] - S[:, :, 1, :]
+                ))
+                loss_bound_top += torch.mean(self.loss_function(
+                    B[:, :, 0, :] - B[:, :, 1, :]
+                ))
 
                 # Closed boundary at the bottom
                 loss_bound_bottom = torch.mean(self.loss_function(
@@ -318,12 +311,12 @@ class PCNNSolver:
                 loss_bound_bottom += torch.mean(self.loss_function(
                     h[:, :, -1, :] - h[:, :, -2, :]
                 ))
-                # loss_bound_bottom += torch.mean(self.loss_function(
-                #     S[:, :, -1, :] - S[:, :, -2, :]
-                # ))
-                # loss_bound_bottom += torch.mean(self.loss_function(
-                #     B[:, :, -1, :] - B[:, :, -2, :]
-                # ))
+                loss_bound_bottom += torch.mean(self.loss_function(
+                    S[:, :, -1, :] - S[:, :, -2, :]
+                ))
+                loss_bound_bottom += torch.mean(self.loss_function(
+                    B[:, :, -1, :] - B[:, :, -2, :]
+                ))
 
                 # Final boundary loss
                 loss_bound = loss_bound_left + loss_bound_right + loss_bound_top + loss_bound_bottom
@@ -331,18 +324,18 @@ class PCNNSolver:
                 #
                 # Regularizers
                 #
-                loss_reg = torch.mean(self.loss_function(
-                    (torch.sum(h_new, dim=(1,2,3)) - torch.sum(h_old, dim=(1,2,3))) / (self.params.width * self.params.height)
-                ))
+                # loss_reg = torch.mean(self.loss_function(
+                #     (torch.sum(h_new, dim=(1,2,3)) - torch.sum(h_old, dim=(1,2,3))) / (self.params.width * self.params.height)
+                # ))
 
                 # Compute combined loss
                 loss = torch.mean(torch.log(
                     self.params.loss_h * loss_h
                     + self.params.loss_momentum * (loss_u + loss_v)
-                    # + self.params.loss_S * loss_S
-                    # + self.params.loss_B * loss_B
+                    + self.params.loss_S * loss_S
+                    + self.params.loss_B * loss_B
                     + self.params.loss_bound * loss_bound
-                    + self.params.loss_reg * loss_reg
+                    # + self.params.loss_reg * loss_reg
                 ))
 
                 # Compute gradients
@@ -361,20 +354,20 @@ class PCNNSolver:
                     loss_h = float(torch.mean(loss_h).detach().cpu().numpy())
                     loss_u = float(torch.mean(loss_u).detach().cpu().numpy())
                     loss_v = float(torch.mean(loss_v).detach().cpu().numpy())
-                    # loss_S = float(torch.mean(loss_S).detach().cpu().numpy())
-                    # loss_B = float(torch.mean(loss_B).detach().cpu().numpy())
+                    loss_S = float(torch.mean(loss_S).detach().cpu().numpy())
+                    loss_B = float(torch.mean(loss_B).detach().cpu().numpy())
                     loss_bound = float(torch.mean(loss_bound).detach().cpu().numpy())
-                    loss_reg = float(torch.mean(loss_reg).detach().cpu().numpy())
+                    # loss_reg = float(torch.mean(loss_reg).detach().cpu().numpy())
                     self.logger.log(f"loss_{self.params.loss}", loss, epoch * self.params.n_batches_per_epoch + i)
                     self.logger.log(f"loss_h_{self.params.loss}", loss_h, epoch * self.params.n_batches_per_epoch + i)
                     self.logger.log(f"loss_u_{self.params.loss}", loss_u, epoch * self.params.n_batches_per_epoch + i)
                     self.logger.log(f"loss_v_{self.params.loss}", loss_v, epoch * self.params.n_batches_per_epoch + i)
-                    # self.logger.log(f"loss_S_{self.params.loss}", loss_S, epoch * self.params.n_batches_per_epoch + i)
-                    # self.logger.log(f"loss_B_{self.params.loss}", loss_B, epoch * self.params.n_batches_per_epoch + i)
+                    self.logger.log(f"loss_S_{self.params.loss}", loss_S, epoch * self.params.n_batches_per_epoch + i)
+                    self.logger.log(f"loss_B_{self.params.loss}", loss_B, epoch * self.params.n_batches_per_epoch + i)
                     self.logger.log(f"loss_bound_{self.params.loss}", loss_bound, epoch * self.params.n_batches_per_epoch + i)
-                    self.logger.log(f"loss_reg_{self.params.loss}", loss_reg, epoch * self.params.n_batches_per_epoch + i)
+                    # self.logger.log(f"loss_reg_{self.params.loss}", loss_reg, epoch * self.params.n_batches_per_epoch + i)
 
-                    print(f"Epoch {epoch}, iter.{i}:\tloss: {round(loss,2)};\tloss_bound: {round(loss_bound,2)};\tloss_h: {round(loss_h,2)};\tloss_u: {round(loss_u,2)};\tloss_v: {round(loss_v,2)};\tloss_reg: {round(loss_reg,2)} \tvRAM allocated: {round(torch.mps.current_allocated_memory()/1000000000.0, 2)}GB")
+                    print(f"Epoch {epoch}, iter.{i}:\tloss: {round(loss,2)};\tloss_bound: {round(loss_bound,2)};\tloss_h: {round(loss_h,2)};\tloss_u: {round(loss_u,2)};\tloss_v: {round(loss_v,2)};\tloss_S: {round(loss_S,2)};\tloss_B: {round(loss_B,2)}; \tvRAM allocated: {round(torch.mps.current_allocated_memory()/1000000000.0, 2)}GB")
                     # if i % 100 == 0:
 
             # Save the training state after each epoch
