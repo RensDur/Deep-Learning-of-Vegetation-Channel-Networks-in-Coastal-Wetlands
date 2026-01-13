5c21826b5aea989f91f3b5c46cd40615e96cab2f
Rens Dur <rensdur@gmail.com>
2026-01-13 08:22:30 +0100

    Prepare simpler problem and allow memory monitoring during job

--- Spline-PINN/dataset.py
+++ Spline-PINN/dataset.py
@@ -41,9 +41,9 @@ class Dataset:
 
         # Variables in this dataset
         self.variables = SplineArray(
-            SplineVariable("h", 2, requires_derivative=True),                           # h describes the zero-meaned surface height, on top of H0
-            SplineVariable("u", 2, requires_derivative=True, requires_laplacian=True),
-            SplineVariable("v", 2, requires_derivative=True, requires_laplacian=True),
+            SplineVariable("h", 1, requires_derivative=True),                           # h describes the zero-meaned surface height, on top of H0
+            SplineVariable("u", 1, requires_derivative=True, requires_laplacian=True),
+            SplineVariable("v", 1, requires_derivative=True, requires_laplacian=True),
             device=self.device
         )
 
