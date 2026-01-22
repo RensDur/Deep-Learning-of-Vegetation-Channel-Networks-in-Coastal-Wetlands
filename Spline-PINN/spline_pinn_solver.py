f8232f23852df24c9da42bf7dffbdf3dc7f6b126
Rens Dur <rensdur@gmail.com>
2026-01-22 10:06:35 +0100

    Resolve reverse use of grad d/dx and d/dy

--- Spline-PINN/spline_pinn_solver.py
+++ Spline-PINN/spline_pinn_solver.py
@@ -102,16 +102,16 @@ class SplinePINNSolver:
 
             # h-loss
             loss_h = loss_h + torch.mean(self.loss_function(
-                dh_dt + (grad_u[:,0:1] + grad_v[:,1:2]) * (h + self.params.H0) + (grad_h[:,0:1]*u + grad_h[:,1:2]*v) + self.params.epsilon * h
+                dh_dt + (grad_u[:,1:2] + grad_v[:,0:1]) * (h + self.params.H0) + (grad_h[:,1:2]*u + grad_h[:,0:1]*v) + self.params.epsilon * h
             ), dim)
 
             # Momentum loss
             loss_u = loss_u + torch.mean(self.loss_function(
-                du_dt - self.params.nu * laplace_u + self.params.grav * grad_h[:,0:1] + self.params.k_epsilon*u + u * grad_u[:,0:1] + v * grad_u[:,1:2] - self.params.f_epsilon * v
+                du_dt - self.params.nu * laplace_u + self.params.grav * grad_h[:,1:2] + self.params.k_epsilon*u + u * grad_u[:,1:2] + v * grad_u[:,0:1] - self.params.f_epsilon * v
             ), dim)
 
             loss_v = loss_v + torch.mean(self.loss_function(
-                dv_dt - self.params.nu * laplace_v + self.params.grav * grad_h[:,1:2] + self.params.k_epsilon*v + u * grad_v[:,0:1] + v * grad_v[:,1:2] - self.params.f_epsilon * u
+                dv_dt - self.params.nu * laplace_v + self.params.grav * grad_h[:,0:1] + self.params.k_epsilon*v + u * grad_v[:,1:2] + v * grad_v[:,0:1] - self.params.f_epsilon * u
             ), dim)
 
             # h boundary condition loss
