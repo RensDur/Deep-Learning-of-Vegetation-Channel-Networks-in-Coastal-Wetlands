a500d15ff57e5f946e1e8fffd4058a64aeaeab26
Rens Dur <rensdur@gmail.com>
2026-01-22 17:52:38 +0100

    Easier folder rename data-processing

--- data-processing/main.py
+++ data-processing/main.py
@@ -2,10 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+folder = "wave-size 0.5"
 
-loss_bound_df = pd.read_csv("./wave-size 0.5/loss_bound.log")
-loss_h_df = pd.read_csv("./wave-size 0.5/loss_h.log")
-loss_momentum_df = pd.read_csv("./wave-size 0.5/loss_momentum.log")
+loss_bound_df = pd.read_csv(f"./{folder}/loss_bound.log")
+loss_h_df = pd.read_csv(f"./{folder}/loss_h.log")
+loss_momentum_df = pd.read_csv(f"./{folder}/loss_momentum.log")
 
 df = pd.merge(loss_bound_df, loss_h_df, on="Index", how='inner')
 df = pd.merge(df, loss_momentum_df, on="Index", how='inner')
