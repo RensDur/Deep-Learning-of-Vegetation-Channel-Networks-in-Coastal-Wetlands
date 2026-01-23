651309ba9870b485aec1c9211d2a7daf506bfd0d
Rens Dur <rensdur@gmail.com>
2026-01-23 12:10:23 +0100

    Add quick script for keywords

---
+++ scripts/keywords.py
@@ -0,0 +1,55 @@
+
+
+pinn_keywords = [
+    "Physics Informed",
+    "Physics Informed Loss",
+    "Theory-Trained Neural Networks",
+    "TTN",
+    "Physics Constrained",
+    "Physics Guided",
+    "Physics Aware",
+    "Physics Governed",
+    "PDE-constrained",
+    "PDE constrained",
+    "Partial Differential Equation",
+    "PDE Solving",
+    "PDE",
+    "Constraing Learning",
+    "Neural PDE solvers",
+    "Neural PDE",
+    "Neural differential equation solvers",
+    "Neural Field",
+]
+
+nn_keywords = [
+    "Neural Network",
+    "Neural Net",
+    "Neural Field",
+    "Artificial Neural Network",
+    "Feed-forward Neural Network",
+    "Convolutional Neural Network",
+    "U-Net",
+    "Deep Learning",
+    "Machine Learning",
+    "Artificial Intelligence",
+    "Optimisation-based learning",
+    "Optimization-based learning",
+    "Multi-layer perceptrons",
+    "Model fitting",
+]
+
+pde_keywords = [
+    "Partial Differential Equation",
+    "PDE Solving",
+    "PDE",
+    "Constraint",
+]
+
+# Make permutations and combinations of these
+pinn_search_segment = " OR ".join([f'\"{kw}\"' for kw in pinn_keywords])
+nn_search_segment = " OR ".join([f'\"{kw}\"' for kw in nn_keywords])
+pde_search_segment = " OR ".join([f'\"{kw}\"' for kw in pde_keywords])
+
+search_term = f"({pinn_search_segment}) AND ({nn_search_segment}) AND ({pde_search_segment}) AND (\"Review\")"
+
+print(search_term)
