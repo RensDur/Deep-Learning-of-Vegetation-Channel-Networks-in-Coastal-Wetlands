cd4441261d7b25190d68643efc4f4ab90dc85122
Rens Dur <rensdur@gmail.com>
2026-01-31 09:36:59 +0100

    Adjust scale of Sediment

--- data-processing/main.py
+++ data-processing/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-folder = "saltmarsh-hs-40-test"
+folder = "saltmarsh-30-jan-hs-64"
 
 loss_bound_df = pd.read_csv(f"./{folder}/loss_bound.log")
 loss_h_df = pd.read_csv(f"./{folder}/loss_h.log")
