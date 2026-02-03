374e0033e30959966a295f3cedeb10e24e5dd37f
Rens Dur <rensdur@gmail.com>
2026-02-03 12:21:20 +0100

    Update scripts

--- data-processing/main.py
+++ data-processing/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-folder = "saltmarsh-test-less-pcgrad-tasks"
+folder = "saltmarsh-test-more-environments"
 
 loss_bound_df = pd.read_csv(f"./{folder}/loss_bound.log")
 loss_h_df = pd.read_csv(f"./{folder}/loss_h.log")
