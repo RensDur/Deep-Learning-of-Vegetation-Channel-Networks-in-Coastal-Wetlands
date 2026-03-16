76bee75a29e8e7401c064a06c01742f6e4fbbdb7
Rens Dur <rensdur@gmail.com>
2026-03-16 12:22:37 +0100

    Deeper network to cross out underfitting

--- Spline-PINN/parameters.py
+++ Spline-PINN/parameters.py
@@ -44,7 +44,7 @@ def params():
 	
 	# Network parameters
 	parser.add_argument('--net', default="ShallowWaterModelUNet", type=str, help='network to train', choices=["Shortcut","Shortcut2","Shortcut2_residual","Shortcut4","Shortcut4_residual","Shortcut3","Fluid_model","Wave_model"])
-	parser.add_argument('--hidden_size', default=32, type=int, help='hidden size of network (default: 20)')
+	parser.add_argument('--hidden_size', default=48, type=int, help='hidden size of network (default: 20)')
 	parser.add_argument('--orders_h', default=1, type=int, help='spline order for water layer thickness [h]')
 	parser.add_argument('--orders_u', default=1, type=int, help='spline order for horizontal momentum [u]')
 	parser.add_argument('--orders_v', default=1, type=int, help='spline order for vertical momentum [v]')
