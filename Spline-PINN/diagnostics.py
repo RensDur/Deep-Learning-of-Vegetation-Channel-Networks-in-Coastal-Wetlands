a232579433b3c59f4475c24dc87cd7d9355ff89d
Rens Dur <rensdur@gmail.com>
2026-03-17 12:30:41 +0100

    Add diagnostics class

---
+++ Spline-PINN/diagnostics.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+from spline_pinn_solver import SplinePINNSolver
+from dataset import Dataset
+import parameters
+import matplotlib.pyplot as plt
+
+
+
+
+
+def main():
+
+
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
