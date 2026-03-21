a2f8dcac00d7d29fa0fd08513d1901a50570dd9f
Rens Dur <rensdur@gmail.com>
2026-03-21 13:18:13 +0100

    Higher spline order for h

--- Spline-PINN/dataset.py
+++ Spline-PINN/dataset.py
@@ -41,7 +41,7 @@ class Dataset:
 
         # Variables in this dataset
         self.variables = SplineArray(
-            SplineVariable("h", 1, requires_derivative=True),                           # h describes the zero-meaned surface height, on top of H0
+            SplineVariable("h", 2, requires_derivative=True),                           # h describes the zero-meaned surface height, on top of H0
             SplineVariable("hu", 2, requires_derivative=True),
             SplineVariable("hv", 2, requires_derivative=True),
             # SplineVariable("s", 1, requires_derivative=True, requires_laplacian=True),
