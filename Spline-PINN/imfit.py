4099e911215307de5125f06419dd6b48d478180c
Rens Dur <rensdur@gmail.com>
2026-04-08 10:20:19 +0200

    Ask to fit 300K

--- Spline-PINN/imfit.py
+++ Spline-PINN/imfit.py
@@ -526,7 +526,7 @@ def evaluation_loop(SELECTED_NUMERICAL_OUTPUT, torch_device):
 
 if __name__ == "__main__":
 
-    SELECTED_NUMERICAL_OUTPUT = 650000
+    SELECTED_NUMERICAL_OUTPUT = 300000
 
     
     # Program mode
