774b99e700834380823265f0965030a41d458791
Rens Dur <rensdur@gmail.com>
2026-04-14 15:42:34 +0200

    More GPU optimized sample calculations

--- Spline-PINN/spline_pinn_solver.py
+++ Spline-PINN/spline_pinn_solver.py
@@ -86,161 +86,212 @@ class SplinePINNSolver:
     
     def compute_batch_loss(self, old_hidden_state, new_hidden_state, grid_offsets, sample_closed_masks, sample_opened_masks, dim=[1,2,3]):
 
-        # Compute Physics Informed Loss image tensor
-        loss_h = 0
-        loss_u = 0
-        loss_v = 0
-        loss_s = 0
-        loss_b = 0
-        loss_bound = 0
-        loss_damp = 0
+
+        # Stack all the masks in batch dimension
+        sample_closed_mask = torch.cat([sample_closed_masks[j] for j in range(len(sample_closed_masks))], dim=0)
+        sample_opened_mask = torch.cat([sample_opened_masks[j] for j in range(len(sample_opened_masks))], dim=0)
+
+        all_samples__h = []
+        all_samples__grad_h = []
+        all_samples__dh_dt = []
+        all_samples__u = []
+        all_samples__grad_u = []
+        all_samples__laplacian_u = []
+        all_samples__du_dt = []
+        all_samples__v = []
+        all_samples__grad_v = []
+        all_samples__laplacian_v = []
+        all_samples__dv_dt = []
+        all_samples__s = []
+        all_samples__grad_s = []
+        all_samples__laplacian_s = []
+        all_samples__ds_dt = []
+        all_samples__b = []
+        all_samples__grad_b = []
+        all_samples__laplacian_b = []
+        all_samples__db_dt = []
+        
 
         # Go over each sample
         for j, sample in enumerate(grid_offsets):
             offset = torch.floor(sample*self.params.resolution_factor)/self.params.resolution_factor
 
-            # For added clarity: The masks define where the BCs act, they're 1 everywhere on the boundary, 0 everywhere else
-            sample_closed_mask = sample_closed_masks[j]
-            sample_opened_mask = sample_opened_masks[j]
-
-            sample_closed_domain_mask = 1-sample_closed_mask
-            sample_opened_domain_mask = 1-sample_opened_mask
-
-            # Put additional border_weight on domain boundaries:
-            # Important: weighed by parameter 'border_weight'
-            # sample_closed_mask = (sample_closed_mask + sample_closed_mask*self.diffuse(sample_closed_domain_mask)*self.params.border_weight).detach()
-            # sample_opened_mask = (sample_opened_mask + sample_opened_mask*self.diffuse(sample_opened_domain_mask)*self.params.border_weight).detach()
-
             # Interpolate spline coefficients to obtain the necessary quantities
             h, grad_h, dh_dt, u, grad_u, laplacian_u, du_dt, v, grad_v, laplacian_v, dv_dt, s, grad_s, laplacian_s, ds_dt, b, grad_b, laplacian_b, db_dt = self.dataset.interpolate_states(old_hidden_state, new_hidden_state, offset)
 
-            # Add mean water level height
-            h_before_relu = h
-            h = F.softplus(h - self.params.Hc) + self.params.Hc
+            # Append these samples to the respective lists
+            all_samples__h.append(h)
+            all_samples__grad_h.append(grad_h)
+            all_samples__dh_dt.append(dh_dt)
+            all_samples__u.append(u)
+            all_samples__grad_u.append(grad_u)
+            all_samples__laplacian_u.append(laplacian_u)
+            all_samples__du_dt.append(du_dt)
+            all_samples__v.append(v)
+            all_samples__grad_v.append(grad_v)
+            all_samples__laplacian_v.append(laplacian_v)
+            all_samples__dv_dt.append(dv_dt)
+            all_samples__s.append(s)
+            all_samples__grad_s.append(grad_s)
+            all_samples__laplacian_s.append(laplacian_s)
+            all_samples__ds_dt.append(ds_dt)
+            all_samples__b.append(b)
+            all_samples__grad_b.append(grad_b)
+            all_samples__laplacian_b.append(laplacian_b)
+            all_samples__db_dt.append(db_dt)
 
-            #
-            # Derive bed friction coefficients
-            #
+        # Stack all interpolations in the right order
+        h =             torch.cat(all_samples__h, dim=0)
+        grad_h =        torch.cat(all_samples__grad_h, dim=0)
+        dh_dt =         torch.cat(all_samples__dh_dt, dim=0)
+        u =             torch.cat(all_samples__u, dim=0)
+        grad_u =        torch.cat(all_samples__grad_u, dim=0)
+        laplacian_u =   torch.cat(all_samples__laplacian_u, dim=0)
+        du_dt =         torch.cat(all_samples__du_dt, dim=0)
+        v =             torch.cat(all_samples__v, dim=0)
+        grad_v =        torch.cat(all_samples__grad_v, dim=0)
+        laplacian_v =   torch.cat(all_samples__laplacian_v, dim=0)
+        dv_dt =         torch.cat(all_samples__dv_dt, dim=0)
+        s =             torch.cat(all_samples__s, dim=0)
+        grad_s =        torch.cat(all_samples__grad_s, dim=0)
+        laplacian_s =   torch.cat(all_samples__laplacian_s, dim=0)
+        ds_dt =         torch.cat(all_samples__ds_dt, dim=0)
+        b =             torch.cat(all_samples__b, dim=0)
+        grad_b =        torch.cat(all_samples__grad_b, dim=0)
+        laplacian_b =   torch.cat(all_samples__laplacian_b, dim=0)
+        db_dt =         torch.cat(all_samples__db_dt, dim=0)
 
-            # n: Manning's coefficient
-            n = self.params.nb  + (self.params.nv - self.params.nb) * b / self.params.k
+        #
+        # PHYSICS INFORMED LOSS CALCULATION
+        #
 
-            # Cz: Chezy coefficient
-            chezy = (1.0 / n) * torch.pow(h, 1.0 / 6.0)
+        # Add mean water level height
+        h_before_relu = h
+        h = F.softplus(h - self.params.Hc) + self.params.Hc
 
-            # Bed friction components
-            # Add really small value to u2+v2 to prevent dividing by zero in backprop (deriv of sqroot is 1/sqrt)
-            tau_precalc = (self.params.grav / torch.pow(chezy, 2)) * torch.pow(torch.pow(u, 2) + torch.pow(v, 2) + 1e-12, 0.5)
-            tau_bx_per_rho = tau_precalc * u
-            tau_by_per_rho = tau_precalc * v
-            tau_b_per_rho  = (self.params.grav / torch.pow(chezy, 2)) * (torch.pow(u, 2) + torch.pow(v, 2))
+        #
+        # Derive bed friction coefficients
+        #
 
-            # Effective water height
-            he = h - self.params.Hc
+        # n: Manning's coefficient
+        n = self.params.nb  + (self.params.nv - self.params.nb) * b / self.params.k
 
-            # Compute topographic diffusion term (\/(Ds\/S)) (Ds is a field)
-            Ds = self.params.D0 * (1.0 - self.params.pD * (b / self.params.k))
-            grad_Ds = - ((self.params.D0 * self.params.pD) / self.params.k) * grad_b
+        # Cz: Chezy coefficient
+        chezy = (1.0 / n) * torch.pow(h, 1.0 / 6.0)
 
-            gradDs_dot_gradS = grad_Ds[:, 1:2] * grad_s[:, 1:2] + grad_Ds[:, 0:1] * grad_s[:, 0:1]
+        # Bed friction components
+        # Add really small value to u2+v2 to prevent dividing by zero in backprop (deriv of sqroot is 1/sqrt)
+        tau_precalc = (self.params.grav / torch.pow(chezy, 2)) * torch.pow(torch.pow(u, 2) + torch.pow(v, 2) + 1e-12, 0.5)
+        tau_bx_per_rho = tau_precalc * u
+        tau_by_per_rho = tau_precalc * v
+        tau_b_per_rho  = (self.params.grav / torch.pow(chezy, 2)) * (torch.pow(u, 2) + torch.pow(v, 2))
 
-            # Full divergence term
-            div_Ds_grad_S = Ds * laplacian_s + gradDs_dot_gradS
+        # Effective water height
+        he = h - self.params.Hc
 
-            #
-            # COMPUTE SAMPLE LOSS
-            #
+        # Compute topographic diffusion term (\/(Ds\/S)) (Ds is a field)
+        Ds = self.params.D0 * (1.0 - self.params.pD * (b / self.params.k))
+        grad_Ds = - ((self.params.D0 * self.params.pD) / self.params.k) * grad_b
 
-            # h-loss
-            loss_h = loss_h + torch.mean(self.loss_function(
-                dh_dt + (u*grad_h[:,1:2] + h*grad_u[:,1:2]) + (v*grad_h[:,0:1] + h*grad_v[:,0:1]) - self.params.Hin
-            ), dim)
+        gradDs_dot_gradS = grad_Ds[:, 1:2] * grad_s[:, 1:2] + grad_Ds[:, 0:1] * grad_s[:, 0:1]
 
-            # Momentum loss
-            loss_u = loss_u + torch.mean(self.loss_function(
-                du_dt + self.params.grav*(grad_h[:,1:2] + grad_s[:,1:2]) + u*grad_u[:,1:2] + v*grad_u[:,0:1] + tau_bx_per_rho/h - self.params.Du * laplacian_u
-            ), dim)
+        # Full divergence term
+        div_Ds_grad_S = Ds * laplacian_s + gradDs_dot_gradS
 
-            loss_v = loss_v + torch.mean(self.loss_function(
-                dv_dt + self.params.grav*(grad_h[:,0:1] + grad_s[:,0:1]) + u*grad_v[:,1:2] + v*grad_v[:,0:1] + tau_by_per_rho/h - self.params.Du * laplacian_v
-            ), dim)
+        #
+        # COMPUTE SAMPLE LOSS
+        #
 
-            # Sediment loss
-            loss_s = loss_s + torch.mean(self.loss_function(
-                ds_dt - self.params.Sin * (he / (self.params.Qs + he)) + self.params.Es * (1.0 - self.params.pE * (b/self.params.k)) * s * tau_b_per_rho - div_Ds_grad_S
-            ), dim)
+        # h-loss
+        loss_h = torch.mean(self.loss_function(
+            dh_dt + (u*grad_h[:,1:2] + h*grad_u[:,1:2]) + (v*grad_h[:,0:1] + h*grad_v[:,0:1]) - self.params.Hin
+        ), dim)
 
-            # Vegetation loss
-            loss_b = loss_b + torch.mean(self.loss_function(
-                db_dt - self.params.r * b * (1.0 - (b/self.params.k)) * (self.params.Qq / (self.params.Qq + he)) + self.params.EB * b * tau_b_per_rho - self.params.DB * laplacian_b
-            ), dim)
+        # Momentum loss
+        loss_u = torch.mean(self.loss_function(
+            du_dt + self.params.grav*(grad_h[:,1:2] + grad_s[:,1:2]) + u*grad_u[:,1:2] + v*grad_u[:,0:1] + tau_bx_per_rho/h - self.params.Du * laplacian_u
+        ), dim)
 
-            #
-            # Boundary condition loss
-            #
+        loss_v = torch.mean(self.loss_function(
+            dv_dt + self.params.grav*(grad_h[:,0:1] + grad_s[:,0:1]) + u*grad_v[:,1:2] + v*grad_v[:,0:1] + tau_by_per_rho/h - self.params.Du * laplacian_v
+        ), dim)
 
-            # Closed boundary
-            loss_bound_closed = torch.mean(sample_closed_mask[:,:,1:-1,1:-1] * self.loss_function(
-                grad_h
-            ), dim)
+        # Sediment loss
+        loss_s = torch.mean(self.loss_function(
+            ds_dt - self.params.Sin * (he / (self.params.Qs + he)) + self.params.Es * (1.0 - self.params.pE * (b/self.params.k)) * s * tau_b_per_rho - div_Ds_grad_S
+        ), dim)
 
+        # Vegetation loss
+        loss_b = torch.mean(self.loss_function(
+            db_dt - self.params.r * b * (1.0 - (b/self.params.k)) * (self.params.Qq / (self.params.Qq + he)) + self.params.EB * b * tau_b_per_rho - self.params.DB * laplacian_b
+        ), dim)
+
+        #
+        # Boundary condition loss
+        #
+
+        # Closed boundary
+        loss_bound_closed = torch.mean(sample_closed_mask[:,:,1:-1,1:-1] * self.loss_function(
+            grad_h
+        ), dim)
+
+        loss_bound_closed = loss_bound_closed + torch.mean(sample_closed_mask[:,:,1:-1,1:-1] * self.loss_function(
+            u
+        ), dim)
+
+        loss_bound_closed = loss_bound_closed + torch.mean(sample_closed_mask[:,:,1:-1,1:-1] * self.loss_function(
+            v
+        ), dim)
+
+        if self.training_sediment:
             loss_bound_closed = loss_bound_closed + torch.mean(sample_closed_mask[:,:,1:-1,1:-1] * self.loss_function(
-                u
+                grad_s
             ), dim)
 
+        if self.training_vegetation:
             loss_bound_closed = loss_bound_closed + torch.mean(sample_closed_mask[:,:,1:-1,1:-1] * self.loss_function(
-                v
+                grad_b / self.params.k  # Normalize any loss related to b by max. carrying capacity to match scale-difference
             ), dim)
 
-            if self.training_sediment:
-                loss_bound_closed = loss_bound_closed + torch.mean(sample_closed_mask[:,:,1:-1,1:-1] * self.loss_function(
-                    grad_s
-                ), dim)
+        # Open boundary
+        loss_bound_open = torch.mean(sample_opened_mask[:,:,1:-1,1:-1] * self.loss_function(
+            grad_h
+        ), dim)
 
-            if self.training_vegetation:
-                loss_bound_closed = loss_bound_closed + torch.mean(sample_closed_mask[:,:,1:-1,1:-1] * self.loss_function(
-                    grad_b / self.params.k  # Normalize any loss related to b by max. carrying capacity to match scale-difference
-                ), dim)
+        loss_bound_open = loss_bound_open + torch.mean(sample_opened_mask[:,:,1:-1,1:-1] * self.loss_function(
+            grad_u
+        ), dim)
 
-            # Open boundary
-            loss_bound_open = torch.mean(sample_opened_mask[:,:,1:-1,1:-1] * self.loss_function(
-                grad_h
-            ), dim)
+        loss_bound_open = loss_bound_open + torch.mean(sample_opened_mask[:,:,1:-1,1:-1] * self.loss_function(
+            grad_v
+        ), dim)
 
+        if self.training_sediment:
             loss_bound_open = loss_bound_open + torch.mean(sample_opened_mask[:,:,1:-1,1:-1] * self.loss_function(
-                grad_u
+                s # S = 0
             ), dim)
 
+        if self.training_vegetation:
             loss_bound_open = loss_bound_open + torch.mean(sample_opened_mask[:,:,1:-1,1:-1] * self.loss_function(
-                grad_v
+                grad_b / self.params.k  # Normalize any loss related to b by max. carrying capacity to match scale-difference
             ), dim)
 
-            if self.training_sediment:
-                loss_bound_open = loss_bound_open + torch.mean(sample_opened_mask[:,:,1:-1,1:-1] * self.loss_function(
-                    s # S = 0
-                ), dim)
+        # Auxilary boundary loss
+        loss_bound_aux = torch.mean(self.loss_function(
+            F.relu(-h_before_relu) # water level thickness can never be negative
+        ), dim)
 
-            if self.training_vegetation:
-                loss_bound_open = loss_bound_open + torch.mean(sample_opened_mask[:,:,1:-1,1:-1] * self.loss_function(
-                    grad_b / self.params.k  # Normalize any loss related to b by max. carrying capacity to match scale-difference
-                ), dim)
-
-            # Auxilary boundary loss
-            loss_bound_aux = torch.mean(self.loss_function(
-                F.relu(-h_before_relu) # water level thickness can never be negative
+        if self.training_sediment:
+            loss_bound_aux = loss_bound_aux + torch.mean(self.loss_function(
+                F.relu(-s) # sedimentary bed elevation can never be negative
             ), dim)
 
-            if self.training_sediment:
-                loss_bound_aux = loss_bound_aux + torch.mean(self.loss_function(
-                    F.relu(-s) # sedimentary bed elevation can never be negative
-                ), dim)
+        if self.training_vegetation:
+            loss_bound_aux = loss_bound_aux + torch.mean(self.loss_function(
+                F.relu(-b) / self.params.k  # Normalize any loss related to b by max. carrying capacity to match scale-difference # vegetation density can never be negative
+            ), dim)
 
-            if self.training_vegetation:
-                loss_bound_aux = loss_bound_aux + torch.mean(self.loss_function(
-                    F.relu(-b) / self.params.k  # Normalize any loss related to b by max. carrying capacity to match scale-difference # vegetation density can never be negative
-                ), dim)
-
-            loss_bound = loss_bound + loss_bound_closed + loss_bound_open + loss_bound_aux
+        loss_bound = loss_bound_closed + loss_bound_open + loss_bound_aux
 
         # Multiply by the loss weights
         loss_h = loss_h * self.params.loss_h
