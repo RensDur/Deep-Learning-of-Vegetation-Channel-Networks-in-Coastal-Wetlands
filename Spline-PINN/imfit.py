9b11a1b8ec1ad8bafd10b073cbcd2843947a5ded
Rens Dur <rensdur@gmail.com>
2026-04-21 10:02:02 +0200

    New imfit batch

--- Spline-PINN/imfit.py
+++ Spline-PINN/imfit.py
@@ -129,9 +129,10 @@ class FitNet(nn.Module):
 
         # Downsampling layers
         self.down1 = nn.Conv2d(self.hidden_size*2, self.hidden_size*2, kernel_size=9, padding=4)  # Maintain resolution, capture large-distance influences
-        self.down2 = nn.Conv2d(self.hidden_size*2, self.hidden_size*2, kernel_size=4, stride=4, padding=0) # Downsample to /4 times the original dimensions
-        self.down3 = nn.Conv2d(self.hidden_size*2, self.hidden_size, kernel_size=2, padding=0)
-        self.down4 = nn.Conv2d(self.hidden_size, out_channels, kernel_size=3, padding=1)
+        self.down2 = nn.Conv2d(self.hidden_size*2, self.hidden_size*2, kernel_size=9, padding=4)  # Maintain resolution, capture large-distance influences
+        self.down3 = nn.Conv2d(self.hidden_size*2, self.hidden_size*2, kernel_size=4, stride=4, padding=0) # Downsample to /4 times the original dimensions
+        self.down4 = nn.Conv2d(self.hidden_size*2, self.hidden_size, kernel_size=2, padding=0)
+        self.down5 = nn.Conv2d(self.hidden_size, out_channels, kernel_size=3, padding=1)
 
         self.output_scalar = output_scalar
 
@@ -161,6 +162,8 @@ class FitNet(nn.Module):
         x = self.down3(x)
         x = torch.relu(x)
         x = self.down4(x)
+        x = torch.relu(x)
+        x = self.down5(x)
 
 
         out = self.output_scalar * torch.tanh(x / self.output_scalar)
@@ -544,21 +547,19 @@ def multi_gpu_training_main(num_gpus=1):
 
     # Split the work
     numerical_outputs_to_process = [
-        40_000,
-        50_000,
-        210_000,
         220_000,
-        260_000,
-        270_000,
-        310_000,
-        380_000,
         430_000,
         460_000,
-        480_000,
-        490_000
+        490_000,
+        550_000,
+        570_000,
+        600_000,
+        610_000,
+        640_000,
+        650_000
     ]
 
-    for i in [j for j in range(520_000, 1_000_000, 10_000)]:
+    for i in [j for j in range(660_000, 1_000_000, 10_000)]:
         numerical_outputs_to_process.append(i)
 
     numerical_outputs_per_gpu = [[] for _ in range(num_gpus)]
@@ -588,7 +589,7 @@ def multi_gpu_training_main(num_gpus=1):
 
 if __name__ == "__main__":
 
-    SELECTED_NUMERICAL_OUTPUT = 40_000
+    SELECTED_NUMERICAL_OUTPUT = 690_000
 
     # Program mode
     mode = "train"
