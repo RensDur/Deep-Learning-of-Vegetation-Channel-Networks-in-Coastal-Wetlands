fe774279aff1bf5208084d6c7ed4a82a995872c3
Rens Dur <rensdur@gmail.com>
2026-07-25 12:16:33 +0200

    Loss B 10e3

--- Spline-PINN/parameters.py
+++ Spline-PINN/parameters.py
@@ -31,7 +31,7 @@ def params():
 	parser.add_argument('--loss_h', default=1, type=float, help='loss factor for wave equation')
 	parser.add_argument('--loss_momentum', default=1, type=float, help='loss factor to connect dz_dt and v')
 	parser.add_argument('--loss_s', default=1e2, type=float, help='loss factor for sediment equation')
-	parser.add_argument('--loss_b', default=1e4, type=float, help='loss factor for sediment equation')
+	parser.add_argument('--loss_b', default=1e3, type=float, help='loss factor for sediment equation')
 	parser.add_argument('--border_weight', default=0, type=float, help='extra weight on fluid domain borders')
 	parser.add_argument('--huber_delta', default=100, type=float, help='Huber Loss: Square loss when |residual| <= threshold, linear outside the threshold')
 
