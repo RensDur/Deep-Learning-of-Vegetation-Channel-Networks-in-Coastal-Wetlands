07b220e3c2af90a1ebb662e210a2d4db6e302d04
Rens Dur <rensdur@gmail.com>
2026-08-17 20:44:57 +0200

    Batch processing per 10 samples

--- Spline-PINN/evaluate_residuals_batch.sh
+++ Spline-PINN/evaluate_residuals_batch.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env zsh
 
 
-python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='0-100' --sfere_start=0 --sfere_end=50
-python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='0-100' --sfere_start=50 --sfere_end=100
 
-python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='100-200' --sfere_start=100 --sfere_end=150
-python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='100-200' --sfere_start=150 --sfere_end=200
 
-python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='200-300' --sfere_start=200 --sfere_end=250
-python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='200-300' --sfere_start=250 --sfere_end=300
+for ((start=0; start<100; start+=10)); do
+    end=$((start + 10))
+    python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='0-100' --sfere_start=$start --sfere_end=$end
+done
 
-python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='300-400' --sfere_start=300 --sfere_end=350
-python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='300-400' --sfere_start=350 --sfere_end=400
+for ((start=100; start<200; start+=10)); do
+    end=$((start + 10))
+    python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='100-200' --sfere_start=$start --sfere_end=$end
+done
 
-python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='400-500' --sfere_start=400 --sfere_end=450
-python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='400-500' --sfere_start=450 --sfere_end=500
+for ((start=200; start<300; start+=10)); do
+    end=$((start + 10))
+    python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='200-300' --sfere_start=$start --sfere_end=$end
+done
+
+for ((start=300; start<400; start+=10)); do
+    end=$((start + 10))
+    python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='300-400' --sfere_start=$start --sfere_end=$end
+done
+
+for ((start=400; start<500; start+=10)); do
+    end=$((start + 10))
+    python main_evaluate_residuals.py --resolution_factor=4 --ablation_model='400-500' --sfere_start=$start --sfere_end=$end
+done
